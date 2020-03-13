6518a2d162579f2fb0aaa045743f670260c097e9
Romy A. J. van Broekhuijsen <romy.mol.vanbroekhuijsen@gmail.com>
2020-03-13 09:52:53 +0000

    items model tested

--- items/tests.py
+++ items/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
-
+from .models import Item
 # Create your tests here.
+
+
+# class ItemTests(TestCase):
+
+#     def test_str(self):
+#         test_name = Item(name='An item')
+#         self.assertEqual(str(test_name), 'An item')
