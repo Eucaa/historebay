587e2b0ff73f45b8882c9b9a752893b900e50731
Romy A. J. van Broekhuijsen <romy.mol.vanbroekhuijsen@gmail.com>
2020-03-17 17:58:16 +0000

    made a more usefull test

--- accounts/tests.py
+++ accounts/tests.py
@@ -4,7 +4,7 @@ from django.test import TestCase
 
 
 class TestViews (TestCase):
-    def test_get_home_page(self):
-        page = self.client.get("/")
+    def test_get_login_page(self):
+        page = self.client.get("/accounts/login/")
         self.assertEqual(page.status_code, 200)
-        self.assertTemplateUsed(page, "registration.html")
+        self.assertTemplateUsed(page, "login.html")
