58cb5bc754c528222b73ea6545c14585f7741b04
Romy A. J. van Broekhuijsen <romy.mol.vanbroekhuijsen@gmail.com>
2020-03-17 13:52:06 +0000

    ulr patterns for cart app added and imported

--- historebay/urls.py
+++ historebay/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from accounts import urls as urls_accounts
 from products import urls as urls_products  # From the products-folder, in the urls.py file, import the urls as urls_products.
 from products.views import all_products  # From the views.py file in the products-folder, import the all_products function.
+from cart import urls as urls_cart  # From the cart folder, import the urls as urls_cart.
+from search import urls as urls_search
 from django.views import static  # Import the static folder (and substance).
 from .settings import MEDIA_ROOT  # From the settings.py file, import the MEDIA_ROOT.
 
@@ -26,5 +28,7 @@ urlpatterns = [
     url(r'^$', all_products, name='index'),  # If there's no name after the slash/ in the URL, then just display all_products of the very first page.
     url(r'^accounts/', include(urls_accounts)),
     url(r'^products/', include(urls_products)),  # Include all our URLs from the products app(folder).
+    url(r'^cart/', include(urls_cart)),
+    url(r'^search/', include(urls_search)),
     url(r'^media/(?P<path>.*)$', static.serve, {'document_root': MEDIA_ROOT}),  # Because of the use of images in media, add this specific (Django-standard) media URL.
 ]
