7157471904969b2e96ee1998a1851e4b227e3efd
Romy A. J. van Broekhuijsen <romy.mol.vanbroekhuijsen@gmail.com>
2020-03-18 19:31:09 +0000

    used setting and urls to check functionality of the new layer to detect black field that are necessary to fill in

--- historebay/urls.py
+++ historebay/urls.py
@@ -20,6 +20,7 @@ from products import urls as urls_products  # From the products-folder, in the u
 from products.views import all_products  # From the views.py file in the products-folder, import the all_products function.
 from cart import urls as urls_cart  # From the cart folder, import the urls as urls_cart.
 from search import urls as urls_search
+from checkout import urls as urls_checkout
 from django.views import static  # Import the static folder (and substance).
 from .settings import MEDIA_ROOT  # From the settings.py file, import the MEDIA_ROOT.
 
@@ -29,6 +30,7 @@ urlpatterns = [
     url(r'^accounts/', include(urls_accounts)),
     url(r'^products/', include(urls_products)),  # Include all our URLs from the products app(folder).
     url(r'^cart/', include(urls_cart)),
+    url(r'^checkout/', include(urls_checkout)),
     url(r'^search/', include(urls_search)),
     url(r'^media/(?P<path>.*)$', static.serve, {'document_root': MEDIA_ROOT}),  # Because of the use of images in media, add this specific (Django-standard) media URL.
 ]
