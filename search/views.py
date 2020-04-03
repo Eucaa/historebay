bad3c1a0efee1a3fdae4a9388825b31ddd68ed54
Romy A. J. van Broekhuijsen <romy.mol.vanbroekhuijsen@gmail.com>
2020-04-03 10:40:43 +0000

    added error message to search option
    if user types in nothing or something that's not available the user will receive an separate error message

--- search/views.py
+++ search/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
 from products.models import Product
 from categories.models import Category
 from productType.models import ProductType
@@ -6,11 +7,20 @@ from productType.models import ProductType
 
 
 def search_filter(request):
-    products = Product.objects.filter(name__icontains=request.GET['q'])  # filter() is a built-in function from Django. name__icontains works like the SQL: SELECT...WHERE name ILIKE etc..
-                                                                         # The form to filter for items is called 'q' (this time). 
-                                                                         # Whatever you type into it will be used to filter for matching products.
+    queryString = request.GET['q']
     categories = all_categories()
     productTypes = all_productTypes()
+
+    if queryString is None or queryString == "":
+        messages.error(request, 'No search input given.')
+        return render(request, "products.html", {"categories": categories, "productTypes": productTypes})
+
+    products = Product.objects.filter(name__icontains=queryString)  # filter() is a built-in function from Django. name__icontains works like the SQL: SELECT...WHERE name ILIKE etc..
+                                                                         # The form to filter for items is called 'q' (this time). 
+                                                                         # Whatever you type into it will be used to filter for matching products.
+    if len(products) == 0:
+        messages.error(request, 'Item(s) not found. Please try again.')
+
     return render(request, "products.html", {'products': products, "categories": categories, "productTypes": productTypes})
 
 
