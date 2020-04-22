3b73bd7a6a7d92885bbd673885bfd5f1027f462a
Romy A. J. van Broekhuijsen <romy.mol.vanbroekhuijsen@gmail.com>
2020-04-22 08:56:05 +0000

    updated email authentication to actually send an email when users fill in a form

--- historebay/settings.py
+++ historebay/settings.py
@@ -169,4 +169,4 @@ EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = 587
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
