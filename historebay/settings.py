ec9679e57f66e72922a2081bb28f8a014c3ebabf
Romy A. J. van Broekhuijsen <romy.mol.vanbroekhuijsen@gmail.com>
2020-04-24 10:03:36 +0000

    changed setting to view on Heroku

--- historebay/settings.py
+++ historebay/settings.py
@@ -96,15 +96,15 @@ WSGI_APPLICATION = 'historebay.wsgi.application'
 
 
 #For Gitpod:
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 # For Heroku:
-# DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))}
+DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))}
 
 
 # Password validation
